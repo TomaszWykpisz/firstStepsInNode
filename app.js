a4272e64ba56f0daed7aa96f440a805a8a6b30d0
TomaszWykpisz <t.wykpisz@gmail.com>
2019-01-13 12:17:34 +0100

    Learn some file system module

--- app.js
+++ app.js
@@ -1,23 +1,56 @@
 ////
 // Module
 
-// Build-in Module
-const http = require('http');
+// // Build-in Module
+// const http = require('http');
 
-// Own Module
-const dataTime = require('./ownModules/dataTime.js');
+// // Own Module
+// const dataTime = require('./ownModules/dataTime.js');
 
-console.log(`Learn module: ${dataTime()}`);
+// console.log(`Learn module: ${dataTime()}`);
 
 ////
 // HTTP Module
 
-http.createServer((req, res) => {
-	res.writeHead(200, {'Content-Type': 'text/html'});
-	res.write('Hello World');
-	res.write('</br>');
-	res.write(`${req.url}`);
-	res.end();
-}).listen(8080);
+// const http = require('http');
+// http.createServer((req, res) => {
+// 	res.writeHead(200, {'Content-Type': 'text/html'});
+// 	res.write('Hello World');
+// 	res.write('</br>');
+// 	res.write(`${req.url}`);
+// 	res.end();
+// }).listen(8080);
 
+////
+// File System
+
+const fs = require('fs');
+
+// Create and add/replace new file
+fs.appendFile('./files/fileSystem/createByappendFile.txt', "Hello I'm appendingFile", (err)=>{
+	if (err) throw err;
+	console.log('Created by appendFile()');
+});
+
+fs.writeFile('./files/fileSystem/createBywriteFile.txt', "Hello I'm writingFle", (err)=>{
+	if (err) throw err;
+	console.log('Created by writeFile()');
+});
+
+//Create empty file
+fs.open('./files/fileSystem/createByopen.txt', 'w', (err, file) => {
+	if (err) throw err;
+	console.log('Created by open()');
+});
+
+// Delete file
+fs.unlink('./files/fileSystem/createByopen.txt', (err) =>{
+	if(err) throw err;
+	console.log('File deleted');
+});
+
+fs.rename('./files/fileSystem/createBywriteFile.txt', './files/fileSystem/createwriteFile.txt', (err) => {
+	if(err) throw err;
+	console.log('File renamed');
+});
 ////
